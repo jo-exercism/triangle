a2c306b60a227807fdb1936ad28ce4315d2b2035
Jacob Ogren <jogren0044@gmail.com>
2020-02-14 12:47:30 -0700

    Refactor solution breaking out checkDuplicates into a helper function

--- triangle.js
+++ triangle.js
@@ -10,17 +10,18 @@ export class Triangle {
     this.side3 = side3
   }
 
-  isEquilateral() {
+  checkDuplicates() {
     let triangleSides = new Array(this.side1, this.side2, this.side3);
+    return triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+  }
 
-    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+  isEquilateral() {
+    let duplicates = this.checkDuplicates();
     return duplicates[0] && duplicates.length === 1 ? true : false;
   }
 
   isIsosceles() {
-    let triangleSides = new Array(this.side1, this.side2, this.side3);
-
-    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+    let duplicates = this.checkDuplicates();
     if(duplicates.length === 2) {
       var validateTriangle = duplicates[0] * 2 >= duplicates[1] && duplicates[1] * 2 >= duplicates[0] ? true : false;
     }
@@ -28,9 +29,7 @@ export class Triangle {
   }
 
   isScalene() {
-    let triangleSides = new Array(this.side1, this.side2, this.side3);
-
-    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+    let duplicates = this.checkDuplicates();
     if(duplicates.length === 3) {
       let sortedArray = duplicates.sort((a, b) => b - a);
       var validateTriangle = sortedArray[1] + sortedArray[2] >= sortedArray[0] ? true : false;
