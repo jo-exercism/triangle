f1e77677f7eda8ac6b20bcd66ed695490be12fe3
Jacob Ogren <jogren0044@gmail.com>
2020-02-14 12:13:05 -0700

    Download Triangle exercism

---
+++ triangle.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1,
+    this.side2 = side2,
+    this.side3 = side3
+  }
+
+  isEquilateral() {
+    let triangleSides = [this.side1, this.side2, this.side3];
+
+    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+    console.log(duplicates)
+    return duplicates.length == 1 ? true : false;
+  }
+
+  isIsosceles() {
+    let triangleSides = [this.side1, this.side2, this.side3];
+
+    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+    return duplicates.length === 2 ? true : false;
+  }
+
+  isScalene() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
