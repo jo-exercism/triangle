c7d7b7ff170d33443787b6dd57495f150cb2f5b3
Jacob Ogren <jogren0044@gmail.com>
2020-02-14 12:45:05 -0700

    Implement isScalene to pass all tests associated with the function

--- triangle.js
+++ triangle.js
@@ -11,15 +11,14 @@ export class Triangle {
   }
 
   isEquilateral() {
-    let triangleSides = [this.side1, this.side2, this.side3];
+    let triangleSides = new Array(this.side1, this.side2, this.side3);
 
     let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
-    console.log(duplicates)
     return duplicates[0] && duplicates.length === 1 ? true : false;
   }
 
   isIsosceles() {
-    let triangleSides = [this.side1, this.side2, this.side3];
+    let triangleSides = new Array(this.side1, this.side2, this.side3);
 
     let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
     if(duplicates.length === 2) {
@@ -29,6 +28,13 @@ export class Triangle {
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    let triangleSides = new Array(this.side1, this.side2, this.side3);
+
+    let duplicates = triangleSides.filter((side, index) => triangleSides.indexOf(side) == index);
+    if(duplicates.length === 3) {
+      let sortedArray = duplicates.sort((a, b) => b - a);
+      var validateTriangle = sortedArray[1] + sortedArray[2] >= sortedArray[0] ? true : false;
+    }
+    return duplicates.length === 3 && validateTriangle ? true : false;
   }
 }
--- triangle.spec.js
+++ triangle.spec.js
@@ -76,27 +76,27 @@ describe('Triangle', () => {
   });
 
   describe('scalene triangle', () => {
-    xtest('no sides are equal', () => {
+    test('no sides are equal', () => {
       const triangle = new Triangle(5, 4, 6);
       expect(triangle.isScalene()).toBe(true);
     });
 
-    xtest('all sides are equal', () => {
+    test('all sides are equal', () => {
       const triangle = new Triangle(4, 4, 4);
       expect(triangle.isScalene()).toBe(false);
     });
 
-    xtest('two sides are equal', () => {
+    test('two sides are equal', () => {
       const triangle = new Triangle(4, 4, 3);
       expect(triangle.isScalene()).toBe(false);
     });
 
-    xtest('may not violate triangle inequality', () => {
+    test('may not violate triangle inequality', () => {
       const triangle = new Triangle(7, 3, 2);
       expect(triangle.isScalene()).toBe(false);
     });
 
-    xtest('sides may be floats', () => {
+    test('sides may be floats', () => {
       const triangle = new Triangle(0.5, 0.4, 0.6);
       expect(triangle.isScalene()).toBe(true);
     });
